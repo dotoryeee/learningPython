eadbac726a4d12802780a3625f4e989ae1960782
dotoryeee <bayer@kakao.com>
2020-12-06 14:58:09 +0900

    bs basics

--- testInLinux.py
+++ testInLinux.py
@@ -1,5 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
-r = requests.get('https://naver.com')
+print()
+result = requests.get('https://kr.indeed.com/jobs?q=%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C&l=')
+soup = BeautifulSoup(result.text, 'html.parser')
 
-print(r)
+pagination = soup.find('div',{'class' : 'pagination'})
+pages = pagination.find_all('a')
+spans = []
+
+for  page in pages:
+    spans.append(page.find('span'))
+
+for i in range(0, len(spans)-1):
+    print(spans[i])
+print()
+
+print(spans[:-1])
