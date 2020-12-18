170de31b0064addddc90796564129a9ca31ab82a
dotoryeee <bayer@kakao.com>
2020-12-19 00:28:04 +0900

    requests

--- nomad/day5.py
+++ nomad/day5.py
@@ -2,5 +2,11 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-os.system("clear")
 url = "https://www.iban.com/currency-codes"
+
+def connect():
+    r = requests.get(url)
+    contents = r.text
+    print(contents)
+
+connect()
